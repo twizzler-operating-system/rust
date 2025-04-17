2a156ad1b4e3ddff4f75832cd2cdc0120f77fa66
Daniel Bittman <danielbittman1@gmail.com>
2025-04-16 19:34:44 -0700

    Fix sysroot.

--- compiler/rustc_target/src/spec/base/twizzler.rs
+++ compiler/rustc_target/src/spec/base/twizzler.rs
@@ -1,15 +1,19 @@
 use crate::spec::{
-    crt_objects, Cc, FramePointer, LinkArgs, LinkOutputKind, LinkerFlavor, Lld, PanicStrategy, TargetOptions, TlsModel, LinkSelfContainedDefault,
+    Cc, FramePointer, LinkArgs, LinkOutputKind, LinkSelfContainedDefault, LinkerFlavor, Lld,
+    PanicStrategy, TargetOptions, TlsModel, crt_objects,
 };
 
-pub(crate) fn opts(static_only: bool) -> TargetOptions {
+pub(crate) fn opts() -> TargetOptions {
     let mut pre_link_args = LinkArgs::new();
+    let mut post_link_args = LinkArgs::new();
     pre_link_args.insert(LinkerFlavor::Gnu(Cc::Yes, Lld::Yes), vec![]);
     pre_link_args.insert(LinkerFlavor::Gnu(Cc::Yes, Lld::No), vec![]);
+    pre_link_args.insert(LinkerFlavor::Gnu(Cc::No, Lld::Yes), vec![]);
+    post_link_args.insert(LinkerFlavor::Gnu(Cc::No, Lld::Yes), vec![]);
 
     TargetOptions {
         os: "twizzler".into(),
-        env: if static_only { "minruntime".into() } else { "".into() },
+        env: "".into(),
         linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
         linker: Some("rust-lld".into()),
         executables: true,
@@ -26,14 +30,15 @@ pub(crate) fn opts(static_only: bool) -> TargetOptions {
             (LinkOutputKind::StaticNoPicExe, &["crtend.o", "crtn.o"]),
             (LinkOutputKind::StaticPicExe, &["crtendS.o", "crtn.o"]),
         ]),
+        post_link_args,
         panic_strategy: PanicStrategy::Unwind,
-        position_independent_executables: !static_only,
-        static_position_independent_executables: !static_only,
-        tls_model: if static_only { TlsModel::LocalExec } else { TlsModel::GeneralDynamic },
-        crt_static_default: static_only,
+        position_independent_executables: true,
+        static_position_independent_executables: true,
+        tls_model: TlsModel::GeneralDynamic,
+        crt_static_default: false,
         crt_static_respected: true,
-        crt_static_allows_dylibs: !static_only,
-        dynamic_linking: !static_only,
+        crt_static_allows_dylibs: true,
+        dynamic_linking: true,
         has_thread_local: true,
         frame_pointer: FramePointer::NonLeaf,
         link_self_contained: LinkSelfContainedDefault::True,
